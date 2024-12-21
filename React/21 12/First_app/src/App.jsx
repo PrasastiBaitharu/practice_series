d3cbabb146d5b28bcd958873dcb7f87ebdbf67a1
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-21 15:49:01 +0530

    Wrapping multiple components using div

--- React/21 12/First_app/src/App.jsx
+++ React/21 12/First_app/src/App.jsx
@@ -1,12 +1,19 @@
-//Displaying Hello world
+//Wrapping multiple components using div inside return
 
 function App(){
-  return <Hello/>
+  return(
+    <div>
+      <Hello/>
+      <WelcomeMessage/>
+    </div>
+  )
 }
 
 function Hello(){
   return <h1>Hello World</h1>
-
+}
+function WelcomeMessage(){
+  return <h1>Welcome to React</h1>
 }
 
 export default App;
