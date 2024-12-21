32b6745775cabcbfea3881c2aebc2d7a638a701d
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-21 17:29:51 +0530

    Creating and accessing nested object in components

--- React/21 12/First_app/src/components/Hello.jsx
+++ React/21 12/First_app/src/components/Hello.jsx
@@ -1,8 +1,13 @@
-//Creating and accessing object in components
+//Creating and accessing nested object in components
 const student = {
   name : "Shyam",
   age : 20,
-  height : 5.4
+  height : 5.4,
+  address : {
+    state : "odisha",
+    city : "sambalpur",
+    pincode: 768550
+  }
 }
 
 function Hello() {
@@ -11,7 +16,7 @@ function Hello() {
       <h1>My name is {student.name}</h1>
       <h1>I am {student.age} Years old</h1>
       <h1>I am {student.height} feet tall</h1>
-      
+      <h1>My location is {student.address.state},{student.address.city},{student.address.pincode}</h1>
     </>
   );
 }
