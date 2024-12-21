d49c52975da97bf948b7524a8e6ab8d39d8ce0f5
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-21 17:22:56 +0530

    Making the global variable to local

--- React/21 12/First_app/src/components/Hello.jsx
+++ React/21 12/First_app/src/components/Hello.jsx
@@ -1,29 +1,32 @@
-//accessing formula in components
-let name  = "Shyam";
-let age = 20;
-let height = 5.4;
-let hobbies = ["Photography" , "Travelling" , "Gaming"];
-function mssg(){
-  return <h1>Good luck for react carrier</h1>
-}
-function Hello(){
-    return(
-      <>
-        <h1>My name is {name}</h1>
-        <h1>I am {age} Years old</h1>
-        <h1>I am {height} feet tall</h1>
-        <h1>My hobbies are -
-          {hobbies.map((hobby)=>{
-          return(
-            <ul>
-            <li>"{hobby}"</li>
-            </ul>
-          )
-        })}</h1>
-        {mssg()}
-        <h3>Total rating = {10+20}</h3>
-      </>
-    )
+//Making the global variable to local
+
+function Hello() {
+  let name = "Shyam";
+  let age = 20;
+  let height = 5.4;
+  let hobbies = ["Photography", "Travelling", "Gaming"];
+  function mssg() {
+    return <h1>Good luck for react carrier</h1>;
   }
+  return (
+    <>
+      <h1>My name is {name}</h1>
+      <h1>I am {age} Years old</h1>
+      <h1>I am {height} feet tall</h1>
+      <h1>
+        My hobbies are -
+        {hobbies.map((hobby) => {
+          return (
+            <ul>
+              <li>"{hobby}"</li>
+            </ul>
+          );
+        })}
+      </h1>
+      {mssg()}
+      <h3>Total rating = {10 + 20}</h3>
+    </>
+  );
+}
 
 export default Hello;
