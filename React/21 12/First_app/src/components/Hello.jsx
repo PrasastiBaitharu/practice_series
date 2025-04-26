c8d29c4093ed99a72c625a1023599e36a364862a
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-04-26 22:08:15 +0530

    Changing the data of the object

--- React/21 12/First_app/src/components/Hello.jsx
+++ React/21 12/First_app/src/components/Hello.jsx
@@ -3,46 +3,37 @@
 function Hello() {
   const student = [
     {
-      name : "Shyam" , 
-      age : 20,
-      height : 5.4,
-      address : {
-        state : "odisha" , 
-        city : "sambalpur" , 
-        pincode : 758965
-      }
+      name: "Shyam",
+      age: 20,
+      height: 5.4,
+      address: {
+        state: "odisha",
+        city: "sambalpur",
+        pincode: 758965,
+      },
     },
     {
-      name : "ram" , 
-      age : 25,
-      height : 5.9,
-      address : {
-        state : "odisha" , 
-        city : "bargarh" , 
-        pincode : 584595
-      }
+      name: "ram",
+      age: 25,
+      height: 5.9,
+      address: {
+        state: "odisha",
+        city: "bargarh",
+        pincode: 584595,
+      },
     },
-    {
-      name : "vyom" , 
-      age : 45,
-      height : 3.4,
-      address : {
-        state : "odisha" , 
-        city : "sonepur" , 
-        pincode : 467848
-      }
-    }
-  ]
-  
+  ];
+
   return (
     <>
-      {student.map((stud)=>{
-       return (
-        <h5>
-        Name - {stud.name} , Age - {stud.age} , Height - {stud.height},
-         Address - "{stud.address.state}" ,"{stud.address.city}" ,"{stud.address.pincode}"
-       </h5>
-       )
+      {student.map((stud) => {
+        return (
+          <h5>
+            Name - {stud.name} , Age - {stud.age} , Height - {stud.height},
+            Address - "{stud.address.state}" ,"{stud.address.city}" ,"
+            {stud.address.pincode}"
+          </h5>
+        );
       })}
     </>
   );
